a7c847f57e0b7585edc453eca4abce20ec9e6173
amirhnajafiz <najafizadeh21@gmail.com>
2022-04-17 15:35:04 +0430

    broker new method implemented

--- internal/broker/config.go
+++ internal/broker/config.go
@@ -1,8 +1,5 @@
 package broker
 
-type Config struct {
-}
-
 func GetConfigs() map[string]string {
 	return map[string]string{
 		"Access-Control-Allow-Origin": "*",
--- internal/broker/sse.go
+++ internal/broker/sse.go
@@ -1,1 +1,7 @@
 package broker
+
+import net "github.com/subchord/go-sse"
+
+func New() *net.Broker {
+	return net.NewBroker(GetConfigs())
+}
