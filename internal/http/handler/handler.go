42b4f3dfdb38d637148ee36f2ce66ff6aef4f863
amirhnajafiz <najafizadeh21@gmail.com>
2022-04-20 12:11:45 +0430

    handler broadcast all events added

--- internal/http/handler/handler.go
+++ internal/http/handler/handler.go
@@ -1,10 +1,42 @@
 package handler
 
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/amirhnajafiz/event-man/internal/broker/client"
+	"github.com/amirhnajafiz/event-man/internal/event"
+)
+
 type Handler struct {
+	Client *client.Client
 }
 
-func (h Handler) PushEvent() {
-	// push
+func (h Handler) PushEvent(writer http.ResponseWriter, request *http.Request) {
+	err := h.Client.Connect(strconv.FormatInt(time.Now().UnixMilli(), 10), writer, request)
+	if err != nil {
+		log.Println(err)
+
+		return
+	}
+
+	var r struct {
+		Data string `json:"data"`
+	}
+
+	err = json.NewDecoder(request.Body).Decode(&r)
+	if err != nil {
+		log.Println(err)
+
+		return
+	}
+
+	h.Client.BroadcastAll(event.New(strconv.FormatInt(time.Now().UnixMilli(), 10), "event", r.Data))
+
+	request.Response.StatusCode = http.StatusOK
 }
 
 func (h Handler) EventHistory() {
