efe168168adda5970ec73b9866de6e58d9bbdeae
amirhnajafiz <najafizadeh21@gmail.com>
2022-04-20 12:34:14 +0430

    server main implemented

--- internal/cmd/server/main.go
+++ internal/cmd/server/main.go
@@ -1,1 +1,21 @@
 package server
+
+import (
+	"net/http"
+
+	"github.com/amirhnajafiz/event-man/internal/broker"
+	"github.com/amirhnajafiz/event-man/internal/broker/client"
+	"github.com/amirhnajafiz/event-man/internal/http/handler"
+)
+
+func New() {
+	h := handler.Handler{
+		Client: &client.Client{
+			Broker: broker.New(),
+		},
+	}
+
+	http.HandleFunc("/connect", h.ClientConnect)
+	http.HandleFunc("/push", h.PushEvent)
+	http.HandleFunc("/push/single", h.PushEventToSingleClient)
+}
