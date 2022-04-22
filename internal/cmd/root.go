a788ef96bb543dc838ee6a1395047303e764aa88
amirhnajafiz <najafizadeh21@gmail.com>
2022-04-22 19:38:42 +0430

    routes added

--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -4,12 +4,20 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"reflect"
 
 	"github.com/amirhnajafiz/event-man/internal/cmd/server"
 )
 
 func Execute() {
 	server.New()
+
+	httpMux := reflect.ValueOf(http.DefaultServeMux).Elem()
+	finList := httpMux.FieldByIndex([]int{1})
+	fmt.Println("Routes:")
+	for _, key := range finList.MapKeys() {
+		fmt.Printf("\t 0.0.0.0:8080%s\n", key)
+	}
 
 	fmt.Println("[Info] Listening on :8080")
 	log.Fatal(http.ListenAndServe(":8080", http.DefaultServeMux))
