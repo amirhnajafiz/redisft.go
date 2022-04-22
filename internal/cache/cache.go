97f221e192335f014d917ed6856433c0cfd2cd24
amirhnajafiz <najafizadeh21@gmail.com>
2022-04-22 23:01:37 +0430

    cache init

---
+++ internal/cache/cache.go
@@ -0,0 +1,13 @@
+package cache
+
+import net "github.com/subchord/go-sse"
+
+var store map[string]net.Event
+
+func Put(e net.Event) {
+	store[e.GetId()] = e
+}
+
+func Pull() map[string]net.Event {
+	return store
+}
