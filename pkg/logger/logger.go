346a1aacc10180d3462f0dcebd1f2b350ac6202a
amirhnajafiz <najafizadeh21@gmail.com>
2022-04-22 19:50:44 +0430

    logger implemented

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,1 +1,18 @@
 package logger
+
+import (
+	"fmt"
+	"log"
+)
+
+func Error(e error) {
+	_ = fmt.Errorf("[ERROR] %s\n", e.Error())
+}
+
+func Fatal(e error) {
+	log.Fatalf("[FATAL] %s\n", e.Error())
+}
+
+func Info(s string, m string) {
+	fmt.Printf("[%s] %s", s, m)
+}
