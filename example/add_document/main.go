2e0c57dc219cfe447d2bdbdff74afdfba17239b4
amirhnajafiz <najafizadeh21@gmail.com>
2024-01-24 12:45:09 +0330

    add: add document

--- example/add_document/main.go
+++ example/add_document/main.go
@@ -1,1 +1,23 @@
-package add_document
+package main
+
+import (
+	"github.com/amirhnajafiz/redisft.go/redisft"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func main() {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	ft := redisft.NewClientFromExistingConnection(rdb)
+
+	if err := ft.Do(redisft.NewQuery(redisft.QueryAdd).
+		WithIndex("myindex").WithParam("docID", "text"),
+	).Err(); err != nil {
+		panic(err)
+	}
+}
