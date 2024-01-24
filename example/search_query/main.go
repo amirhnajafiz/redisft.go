273d844dcf0aacd938e93c7fc55530ec664bf1f9
amirhnajafiz <najafizadeh21@gmail.com>
2024-01-24 12:45:44 +0330

    add: search example

--- example/search_query/main.go
+++ example/search_query/main.go
@@ -1,1 +1,23 @@
-package search_query
+package main
+
+import (
+	"github.com/amirhnajafiz/redisft.go/redisft"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func main() {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	ft := redisft.NewClientFromExistingConnection(rdb)
+
+	if err := ft.Do(redisft.NewQuery(redisft.QuerySearch).
+		WithIndex("myindex").WithParam("query", "*text*"),
+	).Err(); err != nil {
+		panic(err)
+	}
+}
