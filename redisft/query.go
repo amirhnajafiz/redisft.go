2ac478892ffa8f5e290a25113d30bb9ccfce000b
amirhnajafiz <najafizadeh21@gmail.com>
2024-01-25 10:51:51 +0330

    update: query interfaces

--- redisft/query.go
+++ redisft/query.go
@@ -1,28 +1,62 @@
 package redisft
 
-type Query struct {
+type CreateQuery interface {
+	AddField(field, fieldType string) CreateQuery
+}
+
+type AddQuery interface {
+	AddValue(key, value string) AddQuery
+}
+
+type SearchQuery interface {
+	WithQuery(query string) SearchQuery
+}
+
+func NewCreateQuery(index string) CreateQuery {
+	return newQuery(index, QueryCreate)
+}
+
+func NewSearchQuery(index string) CreateQuery {
+	return newQuery(index, QuerySearch)
+}
+
+func NewAddQuery(index string) CreateQuery {
+	return newQuery(index, QueryAdd)
+}
+
+type query struct {
 	index     string
 	params    map[string]string
 	queryType QueryType
 }
 
-func NewQuery(queryType QueryType) Query {
-	return Query{
+func newQuery(index string, queryType QueryType) query {
+	return query{
+		index:     index,
 		queryType: queryType,
+		params:    make(map[string]string),
 	}
 }
 
-func NewAddQuery() Query {
-
-}
-
-func (q Query) WithIndex(index string) Query {
-	q.index = index
+func (q query) AddField(field, fieldType string) CreateQuery {
+	q.withParam(field, fieldType)
 
 	return q
 }
 
-func (q Query) WithParam(key, value string) Query {
+func (q query) AddValue(key, value string) AddQuery {
+	q.withParam(key, value)
+
+	return q
+}
+
+func (q query) WithQuery(query string) SearchQuery {
+	q.withParam("query", query)
+
+	return q
+}
+
+func (q query) withParam(key, value string) query {
 	q.params[key] = value
 
 	return q
